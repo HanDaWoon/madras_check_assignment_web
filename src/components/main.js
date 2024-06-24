2a8ddff87b6fef6e8e9c1fdc3cb9441494635e96
HanDaWoon <bookpia.han@gmail.com>
2024-06-25 01:22:55 +0900

    feat: complete add ip

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { mFetch } from "@/util/MFetch";
-import { Button, Label, Modal, TextInput } from "flowbite-react";
+import AddIpModal from "@/components/addIp";
 
 const Home = () => {
   const [openModal, setOpenModal] = useState(true);
@@ -26,38 +26,6 @@ const Home = () => {
 
   const handleOpenModal = () => {
     setOpenModal(true);
-  };
-
-  const onCloseModal = () => {
-    setOpenModal(false);
-  };
-
-  const handleAddIp = async (e) => {
-    e.preventDefault();
-    console.log(e.target.ip.value);
-    const ip = e.target.ip.value;
-    const memo = e.target.memo.value;
-    const startDate = e.target.startDate.value;
-    const endDate = e.target.endDate.value;
-
-    await mFetch("/ip-access", {
-      credentials: "include",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ipAddress: ip,
-        memo: memo,
-        startDate: startDate,
-        endDate: endDate,
-      }),
-    }).then((res) => {
-      if (res.ok) {
-        fetchIpAccessList();
-        onCloseModal();
-      }
-    });
   };
 
   const fetchIpAccessList = async () => {
@@ -169,35 +137,7 @@ const Home = () => {
                         scope="col"
                         className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-3 focus:outline-none">
-                          <span>IP 주소</span>
-
-                          <svg
-                            className="h-3"
-                            viewBox="0 0 10 11"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              strokeWidth="0.1"
-                            />
-                            <path
-                              d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              strokeWidth="0.1"
-                            />
-                            <path
-                              d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              strokeWidth="0.3"
-                            />
-                          </svg>
-                        </button>
+                        IP 주소
                       </th>
 
                       <th
@@ -315,65 +255,11 @@ const Home = () => {
         </div>
       </section>
 
-      <Modal show={openModal} size="md" onClose={onCloseModal} popup>
-        <Modal.Header />
-        <Modal.Body>
-          <div className="space-y-6">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-              IP 추가
-            </h3>
-            <form className="space-y-6" onSubmit={handleAddIp}>
-              <div className="flex flex-col items-center">
-                <div className="mb-2 block text-left w-full">
-                  <Label htmlFor="ip" value="IP 주소" />
-                </div>
-                <div className="flex flex-row justify-between w-full">
-                  <TextInput
-                    id="ip"
-                    type="text"
-                    placeholder="000.000.000.000"
-                    required
-                  />
-                  <Button size="sm" color="blue">
-                    현재 IP 불러오기
-                  </Button>
-                </div>
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="memo" value="설명" />
-                </div>
-                <TextInput
-                  id="memo"
-                  type="text"
-                  placeholder="IP 주소에 대한 설명을 입력하세요."
-                  required
-                />
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="startDate" value="허용 시작 시간" />
-                </div>
-                <TextInput id="startDate" type="datetime-local" required />
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="endDate" value="하용 끝 시간" />
-                </div>
-                <TextInput id="endDate" type="datetime-local" required />
-              </div>
-              <div className="flex justify-between px-20">
-                <Button size="lg" color="green" type="submit">
-                  저장
-                </Button>
-                <Button size="lg" color="red" onClick={onCloseModal}>
-                  취소
-                </Button>
-              </div>
-            </form>
-          </div>
-        </Modal.Body>
-      </Modal>
+      <AddIpModal
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        fetchIpAccessList={fetchIpAccessList}
+      />
     </div>
   );
 };
