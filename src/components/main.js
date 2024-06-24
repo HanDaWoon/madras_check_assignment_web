5500a35bb5283a0174b631e7a0cfca3e0517f842
HanDaWoon <bookpia.han@gmail.com>
2024-06-25 02:18:46 +0900

    feat: main page ui complete

--- src/components/main.js
+++ src/components/main.js
@@ -2,9 +2,11 @@
 import { useEffect, useState } from "react";
 import { mFetch } from "@/util/MFetch";
 import AddIpModal from "@/components/addIp";
+import { Label, Select, TextInput } from "flowbite-react";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Home = () => {
-  const [openModal, setOpenModal] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
   const [pagination, setPagination] = useState({
     totalRecordCount: 1,
     totalPageCount: 1,
@@ -22,6 +24,13 @@ const Home = () => {
   });
   const [ipAccessList, setIpAccessList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchParam, setSearchParam] = useState({
+    memo: "",
+    startDate: "",
+    endDate: "",
+    page: 1,
+    recordSize: 10,
+  });
 
   const handleOpenModal = () => {
     setOpenModal(true);
@@ -29,8 +38,10 @@ const Home = () => {
 
   const fetchIpAccessList = async () => {
     setLoading(true);
+    const url = "/ip-access";
+
     try {
-      const response = await mFetch("/ip-access", {
+      const response = await mFetch(url, {
         credentials: "include",
       });
       const { data } = await response.json();
@@ -50,19 +61,49 @@ const Home = () => {
   return (
     <div>
       <section className="container px-4 mx-auto h-screen">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <div>
-            <div className="flex items-center gap-x-3">
-              <h2 className="text-lg font-medium text-gray-800 dark:text-white">
-                IP 접근 설정
-              </h2>
-            </div>
-          </div>
+        <h2 className="text-lg pt-4 font-medium text-gray-800">IP 접근 설정</h2>
 
-          <div className="flex items-center mt-4 gap-x-3">
+        <div className="flex justify-between">
+          <div className="flex justify-end items-end gap-4">
+            <Label htmlFor="search_memo" className="text-xs font-medium">
+              <TextInput
+                id="search_memo"
+                className="text-xs font-medium"
+                type="text"
+                icon={IoSearchSharp}
+              />
+            </Label>
+            <Label htmlFor="search_start_date" className="text-xs font-medium">
+              사용 시작 시간
+              <TextInput
+                id="search_start_date"
+                className="text-xs font-medium"
+                type="datetime-local"
+              />
+            </Label>
+            <Label htmlFor="search_end_date" className="text-xs font-medium">
+              사용 끝 시간
+              <TextInput
+                id="search_end_date"
+                className="text-xs font-medium"
+                type="datetime-local"
+              />
+            </Label>
+            <Label htmlFor="record_size" className="text-xs font-medium">
+              리스트 개수
+              <Select id="record_size" required>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+                <option value="25">25</option>
+                <option value="30">30</option>
+              </Select>
+            </Label>
+          </div>
+          <div className="flex items-center gap-x-3">
             <button
               onClick={handleOpenModal}
-              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
+              className="flex items-center justify-center px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 w-auto gap-x-2 hover:bg-blue-600"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -84,48 +125,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="mt-6 md:flex md:items-center md:justify-between">
-          <div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
-            <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
-              View all
-            </button>
-
-            <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-              Monitored
-            </button>
-
-            <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-              Unmonitored
-            </button>
-          </div>
-
-          <div className="relative flex items-center mt-4 md:mt-0">
-            <span className="absolute">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </span>
-
-            <input
-              type="text"
-              placeholder="Search"
-              className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            ></input>
-          </div>
-        </div>
-
-        <div className="flex flex-col mt-6">
+        <div className="flex flex-col mt-3">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
